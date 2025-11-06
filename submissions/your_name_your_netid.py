825fcae588971f0bfcb446fdf61ed98ae51a580e
totallyANT <tataanirudh13@gmail.com>
2025-11-06 22:21:27 +0530

    im probably the winner lol

--- submissions/your_name_your_netid.py
+++ submissions/your_name_your_netid.py
@@ -3,13 +3,13 @@ from ping_game_theory import Strategy, StrategyTester, History, HistoryEntry, Mo
 
 class Bot(Strategy):
     def __init__(self) -> None:
-        self.author_netid = ""  # Your netid here
-        self.strategy_name = ""  # Name of your strategy here
-        self.strategy_desc = ""  # Description of your strategy here
+        self.author_netid = "at612"  # Your netid here
+        self.strategy_name = "I_like_money"  # Name of your strategy here
+        self.strategy_desc = "always defect"  # Description of your strategy here
 
     def begin(self) -> Move:
         # Make your initial move here
-        return Move.COOPERATE  # Example: always starts with COOPERATE (modify it to implement your strategy)
+        return Move.DEFECT  # Example: always starts with COOPERATE (modify it to implement your strategy)
 
     def turn(self, history: History) -> Move:
         # Make your move based on the history
